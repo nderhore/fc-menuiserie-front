d18dd150c459b4888f640b3f70611dc1c3a49850
nderhore <nathan@derhore.fr>
2023-09-28 19:55:32 +0200

    create home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./component/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component: HomeComponent,
+path:'**'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
