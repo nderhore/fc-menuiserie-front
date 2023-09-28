7d6855319f7d08985ed15326c064f693290efb19
nderhore <nathan@derhore.fr>
2023-09-28 20:35:17 +0200

    implements get Menuiserie on startup

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuiserieService} from "../../service/menuiserie/menuiserie.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  public message: string = "Bonjour";
+
+  constructor(private menuiserieService : MenuiserieService) {
+
+  }
+
+  ngOnInit() {
+    this.menuiserieService.getMenuiserie().subscribe(reponse => {
+      console.log(reponse);
+    })
+  }
 
 }
