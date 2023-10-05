d20abd0701d843f3313d3f9dab9f25e4bce1e04c
nderhore <nathan@derhore.fr>
2023-10-05 19:52:46 +0200

    implements attribute on models template

--- src/app/models/service/service.model.ts
+++ src/app/models/service/service.model.ts
@@ -1,2 +1,57 @@
+import {Image} from "../image/image.model";
+
+
 export class Service {
+
+  private _service_id : number;
+
+  private _nom : string;
+
+  private _description : string;
+
+  private _images : Array<Image> = [];
+
+  constructor(data : any) {
+    this._service_id = data.service_id ? data.service_id : null;
+    this._nom = data.nom ? data.nom : null;
+    this._description = data.description ? data.description : null;
+    if(data.images){
+      data.images.forEach((image : Image) => {
+        this._images.push(new Image(image));
+      });
+    }
+  }
+
+
+  get service_id(): number {
+    return this._service_id;
+  }
+
+  set service_id(value: number) {
+    this._service_id = value;
+  }
+
+  get nom(): string {
+    return this._nom;
+  }
+
+  set nom(value: string) {
+    this._nom = value;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+
+  set description(value: string) {
+    this._description = value;
+  }
+
+  get images(): Array<Image> {
+    return this._images;
+  }
+
+  set images(value: Array<Image>) {
+    this._images = value;
+  }
 }
