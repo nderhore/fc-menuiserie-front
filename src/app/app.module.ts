9d93b1b7b91f6669299f7b239b1b78920b56e488
nderhore <nathan@derhore.fr>
2023-10-12 20:53:53 +0200

    add new import for form field module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTooltipModule} from "@angular/material/tooltip";
 import {HttpClientModule} from "@angular/common/http";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import { AdminDashboardComponent } from './component/admin-dashboard/admin-dashboard.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTableModule} from "@angular/material/table";
+import { PopinConfirmationComponent } from './component/popin/popin-confirmation/popin-confirmation.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import { PopinHoraireComponent } from './component/popin/popin-horaire/popin-horaire.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    AdminDashboardComponent,
+    PopinConfirmationComponent,
+    PopinHoraireComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +39,16 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
     BrowserAnimationsModule,
     MatTooltipModule,
     HttpClientModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
