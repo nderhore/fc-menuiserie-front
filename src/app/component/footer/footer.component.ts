cc756e2ba4641141255f5943f26a660c679ad58f
nderhore <nathan@derhore.fr>
2023-10-12 20:52:53 +0200

    add horaire in footer

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import packageJson from '../../../../package.json'
+import {Horaire} from "../../models/horaire/horaire.model";
+import {HoraireService} from "../../service/horaire/horaire.service";
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  // declaration des attributs
+  horaires: Array<Horaire> = [];
   public version: string = packageJson.version;
+
+  constructor(private horaireService : HoraireService) {
+  }
+
+  ngOnInit(): void {
+    this.horaireService.getAllHoraires().subscribe(response => {
+      response.forEach((unHoraire : Horaire) => {
+        this.horaires.push(new Horaire(unHoraire))
+      })
+    })
+  }
+
+  getHeureOuvertureFermeture(horaire: Horaire) {
+
+  }
+
 }
